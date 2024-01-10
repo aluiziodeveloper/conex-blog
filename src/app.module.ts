2cbd59281f1c1b744adea6648067a9c0a165c95e
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-01-10 20:22:37 -0300

    Creating the author model

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import path from 'node:path'
 import { AppResolver } from './app.resolver'
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { AppResolver } from './app.resolver'
       driver: ApolloDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
+    AuthorsModule,
   ],
   providers: [AppService, AppResolver],
 })
