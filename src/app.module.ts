a6292d8d18c8eff4e73c663401c1edf14bb6877a
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-01-10 19:44:20 -0300

    Config graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './database/database.module'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import path from 'path'
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
