13e7cac8e7b05ef3662d57efe237017993fea203
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-01-12 20:38:45 -0300

    Implementing findById int tests and AuthorsDataBuilder helper

--- src/authors/repositories/authors-prisma.repository.int-spec.ts
+++ src/authors/repositories/authors-prisma.repository.int-spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { AuthorsPrismaRepository } from './authors-prisma.repository'
 import { PrismaClient } from '@prisma/client'
 import { execSync } from 'node:child_process'
+import { NotFoundError } from '@/shared/errors/not-found-error'
+import { AuthorDataBuilder } from '../helpers/author-data-builder'
 
 describe('AuthorsPrismaRepository Integration Tests', () => {
   let module: TestingModule
@@ -21,5 +23,26 @@ describe('AuthorsPrismaRepository Integration Tests', () => {
 
   afterAll(async () => {
     await module.close()
+  })
+
+  test('should throws an error when the id is not found', async () => {
+    await expect(
+      repository.findById('796c5a25-1d3b-4228-9a75-06f416c6e218'),
+    ).rejects.toThrow(
+      new NotFoundError(
+        'Author not found using ID 796c5a25-1d3b-4228-9a75-06f416c6e218',
+      ),
+    )
+  })
+
+  test('should find an author by id', async () => {
+    const data = AuthorDataBuilder({})
+
+    const author = await prisma.author.create({
+      data,
+    })
+
+    const result = await repository.findById(author.id)
+    expect(result).toStrictEqual(author)
   })
 })
