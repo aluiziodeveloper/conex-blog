8cda84529bbbe5ecbd28fcdcc3f175cd29edfa8e
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-01-21 14:49:21 -0300

    Implementing CreateAuthorUseCase - part 2

--- src/authors/usecases/create-author.usecase.ts
+++ src/authors/usecases/create-author.usecase.ts
@@ -1,3 +1,7 @@
+import { BadRequestError } from '@/shared/errors/bad-request-error'
+import { AuthorsPrismaRepository } from '../repositories/authors-prisma.repository'
+import { ConflictError } from '@/shared/errors/conflict-error'
+
 export namespace CreateAuthorUsecase {
   export type Input = {
     name: string
@@ -12,6 +16,21 @@ export namespace CreateAuthorUsecase {
   }
 
   export class Usecase {
-    async execute(input: Input): Promise<Output> {}
+    constructor(private authorsRepository: AuthorsPrismaRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const { email, name } = input
+      if (!email || !name) {
+        throw new BadRequestError('Input data not provided')
+      }
+
+      const emailExists = await this.authorsRepository.findByEmail(email)
+      if (emailExists) {
+        throw new ConflictError('Email address used by other author')
+      }
+
+      const author = await this.authorsRepository.create(input)
+      return author
+    }
   }
 }
