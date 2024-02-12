8a5f28b22c747b3f1f2440ec5183830bf3cf1c3e
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-02-12 13:08:58 -0300

    AuthorResolver: register usecases and implementing createAuthor method

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -4,6 +4,10 @@ import { DatabaseModule } from '@/database/database.module'
 import { PrismaService } from '@/database/prisma/prisma.service'
 import { AuthorsPrismaRepository } from './repositories/authors-prisma.repository'
 import { ListAuthorsUsecase } from './usecases/list-authors.usecase'
+import { GetAuthorUsecase } from './usecases/get-author.usecase'
+import { CreateAuthorUsecase } from './usecases/create-author.usecase'
+import { UpdateAuthorUsecase } from './usecases/update-author.usecase'
+import { DeleteAuthorUsecase } from './usecases/delete-author.usecase'
 
 @Module({
   imports: [DatabaseModule],
@@ -24,6 +28,34 @@ import { ListAuthorsUsecase } from './usecases/list-authors.usecase'
       provide: ListAuthorsUsecase.Usecase,
       useFactory: (authosRepository: AuthorsPrismaRepository) => {
         return new ListAuthorsUsecase.Usecase(authosRepository)
+      },
+      inject: ['AuthorsRepository'],
+    },
+    {
+      provide: GetAuthorUsecase.Usecase,
+      useFactory: (authosRepository: AuthorsPrismaRepository) => {
+        return new GetAuthorUsecase.Usecase(authosRepository)
+      },
+      inject: ['AuthorsRepository'],
+    },
+    {
+      provide: CreateAuthorUsecase.Usecase,
+      useFactory: (authosRepository: AuthorsPrismaRepository) => {
+        return new CreateAuthorUsecase.Usecase(authosRepository)
+      },
+      inject: ['AuthorsRepository'],
+    },
+    {
+      provide: UpdateAuthorUsecase.Usecase,
+      useFactory: (authosRepository: AuthorsPrismaRepository) => {
+        return new UpdateAuthorUsecase.Usecase(authosRepository)
+      },
+      inject: ['AuthorsRepository'],
+    },
+    {
+      provide: DeleteAuthorUsecase.Usecase,
+      useFactory: (authosRepository: AuthorsPrismaRepository) => {
+        return new DeleteAuthorUsecase.Usecase(authosRepository)
       },
       inject: ['AuthorsRepository'],
     },
--- src/authors/graphql/resolvers/authors.resolver.ts
+++ src/authors/graphql/resolvers/authors.resolver.ts
@@ -1,14 +1,19 @@
-import { Args, Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { Author } from '../models/author'
 import { Inject } from '@nestjs/common'
 import { ListAuthorsUsecase } from '@/authors/usecases/list-authors.usecase'
 import { SearchParamsArgs } from '../args/search-params.args'
 import { SearchAuthorsResult } from '../models/search-authors-result'
+import { CreateAuthorUsecase } from '@/authors/usecases/create-author.usecase'
+import { CreateAuthorInput } from '../inputs/create-author.input'
 
 @Resolver(() => Author)
 export class AuthorsResolver {
   @Inject(ListAuthorsUsecase.Usecase)
   private listAuthorUseCase: ListAuthorsUsecase.Usecase
+
+  @Inject(CreateAuthorUsecase.Usecase)
+  private createAuthorUseCase: CreateAuthorUsecase.Usecase
 
   @Query(() => SearchAuthorsResult)
   async authors(
@@ -22,5 +27,10 @@ export class AuthorsResolver {
       filter,
     })
     return list
+  }
+
+  @Mutation(() => Author)
+  createAuthor(@Args('data') data: CreateAuthorInput) {
+    return this.createAuthorUseCase.execute(data)
   }
 }
