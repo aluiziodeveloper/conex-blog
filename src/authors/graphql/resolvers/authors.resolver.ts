d65f6065fe001cc59e39d76ab2998669811724ba
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-02-12 12:44:00 -0300

    AuthorResolver: list authors - part 2

--- src/authors/graphql/resolvers/authors.resolver.ts
+++ src/authors/graphql/resolvers/authors.resolver.ts
@@ -3,20 +3,24 @@ import { Author } from '../models/author'
 import { Inject } from '@nestjs/common'
 import { ListAuthorsUsecase } from '@/authors/usecases/list-authors.usecase'
 import { SearchParamsArgs } from '../args/search-params.args'
+import { SearchAuthorsResult } from '../models/search-authors-result'
 
 @Resolver(() => Author)
 export class AuthorsResolver {
   @Inject(ListAuthorsUsecase.Usecase)
   private listAuthorUseCase: ListAuthorsUsecase.Usecase
 
-  @Query(() => [Author])
-  authors(@Args() { page, perPage, sort, sortDir, filter }: SearchParamsArgs) {
-    return this.listAuthorUseCase.execute({
+  @Query(() => SearchAuthorsResult)
+  async authors(
+    @Args() { page, perPage, sort, sortDir, filter }: SearchParamsArgs,
+  ) {
+    const list = await this.listAuthorUseCase.execute({
       page,
       perPage,
       sort,
       sortDir,
       filter,
     })
+    return list
   }
 }
