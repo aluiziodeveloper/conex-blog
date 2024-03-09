ea3dd91ddbfe0908738074818f8123e490a347b3
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-03-09 12:46:42 -0300

    PostsPrismaRepository integration tests - findById

---
+++ src/posts/repositories/posts-prisma.repository.int-spec.ts
@@ -0,0 +1,56 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { PrismaClient } from '@prisma/client'
+import { execSync } from 'node:child_process'
+import { NotFoundError } from '@/shared/errors/not-found-error'
+import { PostsPrismaRepository } from './posts-prisma.repository'
+import { PostsDataBuilder } from '../helpers/posts-data-builder'
+import { AuthorDataBuilder } from '@/authors/helpers/author-data-builder'
+
+describe('PostsPrismaRepository Integration Tests', () => {
+  let module: TestingModule
+  let repository: PostsPrismaRepository
+  const prisma = new PrismaClient()
+
+  beforeAll(async () => {
+    execSync('npm run prisma:migratetest')
+    await prisma.$connect()
+    module = await Test.createTestingModule({}).compile()
+    repository = new PostsPrismaRepository(prisma as any)
+  })
+
+  beforeEach(async () => {
+    await prisma.post.deleteMany()
+  })
+
+  afterAll(async () => {
+    await module.close()
+  })
+
+  test('should throws an error when the id is not found', async () => {
+    await expect(
+      repository.findById('796c5a25-1d3b-4228-9a75-06f416c6e218'),
+    ).rejects.toThrow(
+      new NotFoundError(
+        'Post not found using ID 796c5a25-1d3b-4228-9a75-06f416c6e218',
+      ),
+    )
+  })
+
+  test('should find a post by id', async () => {
+    const postData = PostsDataBuilder({})
+    const authorData = AuthorDataBuilder({})
+    const author = await prisma.author.create({ data: authorData })
+
+    const post = await prisma.post.create({
+      data: {
+        ...postData,
+        author: {
+          connect: { ...author },
+        },
+      },
+    })
+
+    const result = await repository.findById(post.id)
+    expect(result).toStrictEqual(post)
+  })
+})
