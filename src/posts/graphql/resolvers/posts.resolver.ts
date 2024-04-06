d84e4b30dbaaab7ec3c75dd061b38f448b8eee41
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-04-06 11:52:42 -0300

    PostResolver: implementing getPostById method

--- src/posts/graphql/resolvers/posts.resolver.ts
+++ src/posts/graphql/resolvers/posts.resolver.ts
@@ -1,9 +1,18 @@
-import { Args, Mutation, Parent, ResolveField, Resolver } from '@nestjs/graphql'
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql'
 import { Post } from '../models/post'
 import { Inject } from '@nestjs/common'
 import { CreatePostUseCase } from '@/posts/usecases/create-post.usecase'
 import { CreatePostInput } from '../inputs/create-post.input'
 import { GetAuthorUsecase } from '@/authors/usecases/get-author.usecase'
+import { GetPostUseCase } from '@/posts/usecases/get-post.usecase'
+import { PostIdArgs } from '../args/post-id.args'
 
 @Resolver(() => Post)
 export class PostsResolver {
@@ -12,6 +21,14 @@ export class PostsResolver {
 
   @Inject(GetAuthorUsecase.Usecase)
   private getAuthorUseCase: GetAuthorUsecase.Usecase
+
+  @Inject(GetPostUseCase.UseCase)
+  private getPostUseCase: GetPostUseCase.UseCase
+
+  @Query(() => Post)
+  async getPostById(@Args() { id }: PostIdArgs) {
+    return this.getPostUseCase.execute({ id })
+  }
 
   @Mutation(() => Post)
   async createPost(@Args('data') data: CreatePostInput) {
