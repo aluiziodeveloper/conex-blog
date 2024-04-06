1853b898e1bdb38911352186bcb284fdbb40fb26
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-04-06 09:49:57 -0300

    Implementing GetPostUseCase

---
+++ src/posts/usecases/get-post.usecase.ts
@@ -0,0 +1,26 @@
+import { PostOutput } from '../dto/post-output'
+import { PostsPrismaRepository } from '../repositories/posts-prisma.repository'
+
+export namespace GetPostUseCase {
+  export type Input = {
+    id: string
+  }
+
+  export type Output = PostOutput
+
+  export class UseCase {
+    constructor(private postsRepository: PostsPrismaRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const post = await this.postsRepository.findById(input.id)
+      return {
+        id: post.id,
+        title: post.title,
+        content: post.content,
+        published: post.published,
+        authorId: post.authorId,
+        createdAt: post.createdAt,
+      }
+    }
+  }
+}
