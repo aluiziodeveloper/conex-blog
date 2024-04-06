cdfcec56c7ee9d8ff8246a97506d6575a7e4c58d
Aluizio Developer <aluiziodeveloper@gmail.com>
2024-04-06 10:20:50 -0300

    Registering usecases in PostsModule

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,6 +2,11 @@ import { DatabaseModule } from '@/database/database.module'
 import { PrismaService } from '@/database/prisma/prisma.service'
 import { Module } from '@nestjs/common'
 import { PostsPrismaRepository } from './repositories/posts-prisma.repository'
+import { CreatePostUseCase } from './usecases/create-post.usecase'
+import { AuthorsPrismaRepository } from '@/authors/repositories/authors-prisma.repository'
+import { GetPostUseCase } from './usecases/get-post.usecase'
+import { PublishPostUseCase } from './usecases/publish-post.usecase'
+import { UnpublishPostUseCase } from './usecases/unpublish-post.usecase'
 
 @Module({
   imports: [DatabaseModule],
@@ -16,6 +21,44 @@ import { PostsPrismaRepository } from './repositories/posts-prisma.repository'
         return new PostsPrismaRepository(prismaService)
       },
       inject: ['PrismaService'],
+    },
+    {
+      provide: 'AuthorsRepository',
+      useFactory: (prismaService: PrismaService) => {
+        return new AuthorsPrismaRepository(prismaService)
+      },
+      inject: ['PrismaService'],
+    },
+    {
+      provide: CreatePostUseCase.UseCase,
+      useFactory: (
+        postsRepository: PostsPrismaRepository,
+        authorsRepository: AuthorsPrismaRepository,
+      ) => {
+        return new CreatePostUseCase.UseCase(postsRepository, authorsRepository)
+      },
+      inject: ['PostsRepository', 'AuthorsRepository'],
+    },
+    {
+      provide: GetPostUseCase.UseCase,
+      useFactory: (postsRepository: PostsPrismaRepository) => {
+        return new GetPostUseCase.UseCase(postsRepository)
+      },
+      inject: ['PostsRepository'],
+    },
+    {
+      provide: PublishPostUseCase.UseCase,
+      useFactory: (postsRepository: PostsPrismaRepository) => {
+        return new PublishPostUseCase.UseCase(postsRepository)
+      },
+      inject: ['PostsRepository'],
+    },
+    {
+      provide: UnpublishPostUseCase.UseCase,
+      useFactory: (postsRepository: PostsPrismaRepository) => {
+        return new UnpublishPostUseCase.UseCase(postsRepository)
+      },
+      inject: ['PostsRepository'],
     },
   ],
 })
